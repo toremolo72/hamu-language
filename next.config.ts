ae9c018cf2ebf1da927c74c8ecfd7cda71f6f580
toremolo72 <toremolo72@digitalnature.slis.tsukuba.ac.jp>
2024-12-14 00:11:21 +0900

    chotto fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
 };
 
 export default nextConfig;
-
-module.exports = {
-  experimental: {
-    outputStandalone: true, // 出力設定を調整
-  },
-};
